938a732f95f959271f5a421bcc438cf2ad2595d9
Thibault Benedito <thibaultbenedito@gmail.com>
2021-10-11 16:14:27 +0200

    final version of FixedVulnerable.sol

--- security/FixedVulnerable.sol
+++ security/FixedVulnerable.sol
@@ -1,45 +1,49 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.9;
- 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
- 
+
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 contract Crowdsale {
-   using SafeMath for uint256;
- 
-   address public owner; // the owner of the contract
-   address payable public escrow; // wallet to collect raised ETH
-   uint256 public savedBalance; // Total amount raised in ETH
-   mapping (address => uint256) public balances; // Balances in incoming Ether
- 
-   // Initialization
-   constructor(address payable _escrow) {
-       owner = msg.sender;
-       // add address of the specific contract
-       escrow = _escrow;
-   }
-  
+  using SafeMath for uint256;
+
+  address public owner; // the owner of the contract
+  address payable public escrow; // wallet to collect raised ETH
+  uint256 public savedBalance = 0; // Total amount raised in ETH
+  mapping (address => uint256) public balances; // Balances in incoming Ether
+  // Event to record each time Ether is paid out
+  event PayEther(
+  address indexed _receiver,
+  uint256 indexed _value,
+  uint256 indexed _timestamp
+  );
+
+  // Initialization
+  constructor (address payable _escrow) public{
+      owner = msg.sender;
+      // add address of the specific contract
+      escrow = _escrow;
+  }
+
    // function to receive ETH
-   
-   
-   receive() external payable {
-       balances[msg.sender] = balances[msg.sender].add(msg.value);
-       savedBalance = savedBalance.add(msg.value);
-       
-       (bool sent, ) = escrow.call{value: msg.value}("");
-        require(sent, "Failed to receive Ether");
-   }
-  
+  receive() payable external {
+      balances[msg.sender] = balances[msg.sender].add(msg.value);
+      savedBalance = savedBalance.add(msg.value);
+      escrow.transfer(msg.value);
+      emit PayEther(escrow, msg.value, now);
+  }
+
    // refund investisor
-   function withdrawPayments() public {
-       require(balances[msg.sender] > 0, "Null balance");
-       address payable payee = msg.sender;
-       uint256 payment = balances[payee];
- 
-       savedBalance = savedBalance.sub(payment);
-       delete balances[payee];
-       
-       (bool sent, ) = payee.call{value: payment}("");
-        require(sent, "Failed to send Ether");
-   }
+  function withdrawPayments() public{
+      address payable payee = msg.sender;
+      uint256 payment = balances[payee];
+      require(payment != 0);
+      require(address(this).balance >= payment);
+    
+      savedBalance = savedBalance.sub(payment);
+      balances[payee] = 0;
+    
+      payee.transfer(payment);
+      emit PayEther(payee, payment, now);
+  }
 }
